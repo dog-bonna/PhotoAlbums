938827359f21e9af96786122c474535bb00548c8
Damian Ogbonna Jr <damianog@buffalo.edu>
2021-01-27 11:52:23 -0500

    set default page to PhotoAlbum page

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace PhotoAlbums
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=PhotoAlbum}/{action=Index}/{id?}");
             });
         }
     }
